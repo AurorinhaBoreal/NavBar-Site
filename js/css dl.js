5eb7bd9f59fed3a6f3fdd1215a6ac7f2cf86b57c
Aurora <auro.kruschewsky@gmail.com>
2023-06-20 07:51:16 -0300

    CSS Light/Dark Page

--- js/css dl.js
+++ js/css dl.js
@@ -3,9 +3,9 @@
 function changeTheme() {
     const body = document.querySelector("body");
     const button = document.querySelector(".ctButton");
-    const navbar = document.querySelector(".navbar-color");
+    const navbar = document.querySelector(".navbarColor");
 
-    body.classList.toggle("dark");
+    body.classList.toggle('dark');
     button.classList.toggle('dark');
     navbar.classList.toggle('dark');
 }
